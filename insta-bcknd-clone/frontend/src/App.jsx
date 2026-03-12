ff910ecb751fc7f5b3185f7421377a79478ea67a
devMayank1005 <devmayank1005@gmail.com>
2026-03-12 21:59:21 +0530

    ifxed pat issue

--- insta-bcknd-clone/frontend/src/App.jsx
+++ insta-bcknd-clone/frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { RouterProvider } from "react-router"
 import { router } from "./app.routes"
 import { AuthProvider } from "./features/auth/auth.context"
 import "./features/shared/global.scss"
-import { PostContextProvider } from "./features/posts/Post.context"
+import { PostContextProvider } from "./features/posts/post.context"
 import UserProfileSidebar from "./features/shared/components/UserProfileSidebar"
 
 function App() {
